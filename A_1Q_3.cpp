9e76ed3ee9f2d3aeb08c98d31fdfa8bb84a8c7e9
Madhur <Madhur>
2023-09-23 18:39:46 +0530

    Assignment 1 Question 3 uploaded

--- A_1Q_3.cpp
+++ A_1Q_3.cpp
@@ -1,3 +1,9 @@
+/*
+Q3. Write a menu driven program for Student in CPP language. Create a class student with data members roll no, name and marks. Implement the following functions
+void initStudent();
+void printStudentOnConsole();
+void acceptStudentFromConsole();
+*/
 #include<iostream>
 #include<cstring>
 using namespace std;
